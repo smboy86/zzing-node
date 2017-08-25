f2f8b125e91a35c3618c736543734e622d2d6f85
SeongMin Park <smboy86@naver.com>
2017-08-25 05:57:08 +0000

    170825 read books

--- Learning-Node(book_origin)/chap3/example1.js
+++ Learning-Node(book_origin)/chap3/example1.js
@@ -3,3 +3,9 @@ process.stdin.resume();
 process.stdin.on('data', function (chunk) {
   process.stdout.write('data: ' + chunk);
 });
+
+
+// 170825 smPark 최초 실행
+// 위에 선언된 것도 없고... 아무런 형식도 없는데
+// 바로 Run 해서 실행이 되는구나.. 
+// 정말 단순하다 노드 ㄷㄷㄷ
